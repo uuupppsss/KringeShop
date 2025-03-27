fec528b6bc1b7690a2d3f584e093704b5c61f3e8
uuupppsss <ulya.ch.06@mail.ru>
2025-03-27 21:19:26 +1000

    ffff

--- KringeShopWeb/Services/UserService.cs
+++ KringeShopWeb/Services/UserService.cs
@@ -1,17 +1,19 @@
 ﻿using KringeShopLib.Model;
-namespace KringeShopWeb.Services;
 
-public class UserService
+namespace KringeShopWeb.Services
 {
-    public User CurrentUser { get; private set; }
-
-    public void SetCurrentUser(User user)
+    public class UserService
     {
-        CurrentUser = user;
-    }
+        public User CurrentUser { get; private set; }
 
-    public void ClearCurrentUser()
-    {
-        CurrentUser = null;
+        public void SetCurrentUser(User user)
+        {
+            CurrentUser = user;
+        }
+
+        public void ClearCurrentUser()
+        {
+            CurrentUser = null;
+        }
     }
 }
