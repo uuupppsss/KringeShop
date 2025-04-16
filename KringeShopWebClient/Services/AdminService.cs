2d7c305184e17c7877e5f1b218d482f55ca52b46
kuren0k <gg_awp_578@mail.ru>
2025-04-16 16:45:56 +1000

    картинки должно работать

--- KringeShopWebClient/Services/AdminService.cs
+++ KringeShopWebClient/Services/AdminService.cs
@@ -1,4 +1,6 @@
 ﻿using KringeShopLib.Model;
+using System.Text;
+using System.Text.Json;
 
 namespace KringeShopWebClient.Services
 {
@@ -12,7 +14,35 @@ namespace KringeShopWebClient.Services
        
         public async Task AddProduct(ProductDTO product, List<byte[]> images)
         {
+            
+            try
+            {
+                int addedProductId=0;
+                string json = JsonSerializer.Serialize(product);
+                var responce = await client.PostAsync("Products", new StringContent(json, Encoding.UTF8, "application/json"));
+                if (!responce.IsSuccessStatusCode)
+                {
+                  //ошибка
+                }
+                else
+                {
+                    addedProductId = await responce.Content.ReadFromJsonAsync<int>();
+                    string imajes_json = JsonSerializer.Serialize(images);
+                    var images_responce = await client.PostAsync($"Products/Images{addedProductId}", new StringContent(imajes_json, Encoding.UTF8, "application/json"));
+                    if (!responce.IsSuccessStatusCode)
+                    {
+                        //ошибка
+                    }
+                    else
+                    {
+                        //успех
+                    }
+                }
+            }
+            catch
+            {
 
+            }
         }
 
 
